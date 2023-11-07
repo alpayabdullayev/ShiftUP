b150107e53134004310cfa426964ac47add39af1
alpayabdullayev <alpayabdullayev0@gmail.com>
2023-11-07 23:16:27 +0400

    config 7.1scss, added header-top

---
+++ assets/js/scripts.js
@@ -0,0 +1,5 @@
+// const cursor = document.querySelector('.cursor');
+
+// document.addEventListener('mousemove', function(x) {
+//     cursor.style.cssText = "left: " + x.clientX + "px; top: " + x.clientY + "px;";
+// });
