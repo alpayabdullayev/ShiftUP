98171e833554ed843c0127e3096b11046d0d94e6
alpayabdullayev <alpayabdullayev0@gmail.com>
2023-11-09 00:52:13 +0400

    added Navbar,mob Nav,sticky Nav,slider and bugFixed

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -11,25 +11,35 @@
 //     this.classList.remove('active');
 // });
 
+window.addEventListener("scroll", function () {
+  let position = window.scrollY;
+  let navbar2 = document.getElementById("navbar2");
+  let screenWidth = window.innerWidth; // Ekran genişliğini al
 
-const offCanvas = document.getElementById("offCanvas");
-
-
-const offCanvasOpenBtn = document.querySelector(".spans");
-const offCanvasCloseBtn = document.getElementById("offcanvasCloseBtn");
-
-
-offCanvas.style.transform = "translateX(100%)";
-
-function openOffCanvas() {
-
-  offCanvas.style.transform = "translateX(0)";
-}
-
-offCanvasOpenBtn.addEventListener("click", openOffCanvas);
-offCanvasCloseBtn.addEventListener("click", () => {
-  offCanvas.style.transform = "translateX(100%)";
+  if (screenWidth > 991) {
+    if (position > 170) {
+      navbar2.style.display = "block";
+      navbar2.style.position = "fixed";
+      navbar2.style.top = "0";
+      navbar2.style.left = "0";
+      navbar2.style.width = "100%";
+      navbar2.style.backgroundColor = "#fff";
+    } else {
+      navbar2.style.display = "none";
+    }
+  } else {
+    navbar2.style.display = "block";
+    navbar2.style.position = "fixed";
+    navbar2.style.top = "0";
+    navbar2.style.left = "0";
+    navbar2.style.width = "100%";
+    navbar2.style.backgroundColor = "#fff";
+  }
 });
+
+
+
+
 
 
 
@@ -44,6 +54,7 @@ offCanvasCloseBtn.addEventListener("click", () => {
 const offCanvasSideBar = document.getElementById("offCanvasSideBar")
 
 const openOffCanvasSideBar = document.querySelector(".hamburger")
+const openOffCanvasSideBarFiexed = document.querySelector(".hamburgerFixed")
 
 const offcanvasCloseBtnSideBar = document.getElementById("offcanvasCloseBtnSideBar")
 
@@ -52,9 +63,12 @@ offCanvasSideBar.style.transform = "translateX(100%)"
 function openOffCanvasSideBarFunction() {
     offCanvasSideBar.style.transform = "translateX(0)"
 }
-openOffCanvasSideBar.addEventListener("click",()=>{
+openOffCanvasSideBarFiexed.addEventListener("click",()=>{
     openOffCanvasSideBarFunction()
     console.log("salam");
+} );
+openOffCanvasSideBar.addEventListener("click",()=>{
+  openOffCanvasSideBarFunction()
 } );
 
 
@@ -64,3 +78,81 @@ offcanvasCloseBtnSideBar.addEventListener("click", () => {
 });
   
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const offCanvas = document.getElementById("offCanvas");
+
+
+const offCanvasOpenBtn = document.querySelector(".spans");
+const offCanvasOpenBtnFixed = document.querySelector(".spansFixed");
+const offCanvasCloseBtn = document.getElementById("offcanvasCloseBtn");
+
+
+offCanvas.style.transform = "translateX(100%)";
+
+
+
+offCanvasOpenBtn.addEventListener("click", openOffCanvas);
+offCanvasOpenBtnFixed.addEventListener("click", openOffCanvas);
+offCanvasCloseBtn.addEventListener("click", () => {
+  offCanvas.style.transform = "translateX(100%)";
+});
+
+
+
+
+
+
+const sideBar = document.getElementById("sideBar");
+const sideBarOpenBtn = document.getElementById("hamburgerFixedSideBar");
+const sideBarCloseBtn = document.getElementById("sideBarCloseBtn");
+
+
+sideBar.style.transform = "translateY(-900px)";
+
+function openSideBar() {
+  offCanvas.style.transform = "translateX(100%)";
+  sideBar.style.transform = "translateY(0)";
+}
+
+function openOffCanvas() {
+  sideBar.style.transform = "translateY(-900px)";
+  offCanvas.style.transform = "translateX(0)";
+}
+
+
+sideBarOpenBtn.addEventListener("click", openSideBar);
+sideBarCloseBtn.addEventListener("click", () => {
+  sideBar.style.transform = "translateY(-900px)";
+});
+
+
+
+
+
+const mobileNavlink = document.querySelectorAll('.mobile-navlink');
+const sideBarMenu = document.querySelectorAll('.sideBarMenu');
+
+mobileNavlink.forEach((item, index) => {
+  item.addEventListener('click', () => {
+    sideBarMenu[index].classList.toggle('active');
+
+    sideBarMenu.forEach((element, i) => {
+      if (index !== i) {
+        sideBarMenu[i].classList.remove('active');
+      }
+    });
+  });
+});
