f9979eab5321d0259b16dd44d0d3277f762b6b39
alpayabdullayev <alpayabdullayev0@gmail.com>
2023-11-08 19:13:46 +0400

    added Header adn Slider Section

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,3 +3,64 @@
 // document.addEventListener('mousemove', function(x) {
 //     cursor.style.cssText = "left: " + x.clientX + "px; top: " + x.clientY + "px;";
 // });
+// document.querySelector('.beforeItem2 a').addEventListener('mouseover', function() {
+//     this.classList.add('active');
+// });
+
+// document.querySelector('.beforeItem2 a').addEventListener('mouseout', function() {
+//     this.classList.remove('active');
+// });
+
+
+const offCanvas = document.getElementById("offCanvas");
+
+
+const offCanvasOpenBtn = document.querySelector(".spans");
+const offCanvasCloseBtn = document.getElementById("offcanvasCloseBtn");
+
+
+offCanvas.style.transform = "translateX(100%)";
+
+function openOffCanvas() {
+
+  offCanvas.style.transform = "translateX(0)";
+}
+
+offCanvasOpenBtn.addEventListener("click", openOffCanvas);
+offCanvasCloseBtn.addEventListener("click", () => {
+  offCanvas.style.transform = "translateX(100%)";
+});
+
+
+
+
+
+
+
+
+
+
+
+const offCanvasSideBar = document.getElementById("offCanvasSideBar")
+
+const openOffCanvasSideBar = document.querySelector(".hamburger")
+
+const offcanvasCloseBtnSideBar = document.getElementById("offcanvasCloseBtnSideBar")
+
+offCanvasSideBar.style.transform = "translateX(100%)"
+
+function openOffCanvasSideBarFunction() {
+    offCanvasSideBar.style.transform = "translateX(0)"
+}
+openOffCanvasSideBar.addEventListener("click",()=>{
+    openOffCanvasSideBarFunction()
+    console.log("salam");
+} );
+
+
+
+offcanvasCloseBtnSideBar.addEventListener("click", () => {
+    offCanvasSideBar.style.transform = "translateX(100%)";
+});
+  
+
